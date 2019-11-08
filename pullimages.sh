677b8848da487dbe39ef86e2591277a4939a3f85
wurenxi <wurenxi@dataclouds.com.cn>
2019-11-08 14:50:53 +0800

    `````

--- pullimages.sh
+++ pullimages.sh
@@ -1,3 +1,10 @@
 #/bin/bash
 
 curl http://metadata.google.internal/computeMetadata/v1/instance/zone -H "Metadata-Flavor: Google"
+
+git config --global user.name $1
+git config --global user.password $2
+
+git add -A
+git commit -m 'add google script'
+git push origin master
